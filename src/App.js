40f89dfe51aa352d8046f048e90626396e9cddbe
Jahid <jahidulhaque767@gmail.com>
2021-10-14 08:27:39 +0600

    added auth provider

--- src/App.js
+++ src/App.js
@@ -4,14 +4,17 @@ import NavBar from "./components/NavBar/NavBar";
 import Hero from "./components/Hero/Hero";
 import ChooseUs from "./components/ChooseUs/ChooseUs";
 import Footer from "./components/Footer/Footer";
+import AuthProvider from "./context/AuthProvider";
 
 function App() {
   return (
     <div>
-      <NavBar></NavBar>
-      <Hero></Hero>
-      <ChooseUs></ChooseUs>
-      <Footer></Footer>
+      <AuthProvider>
+        <NavBar></NavBar>
+        <Hero></Hero>
+        <ChooseUs></ChooseUs>
+        <Footer></Footer>
+      </AuthProvider>
     </div>
   );
 }
---
+++ src/hooks/useFirebase.js
@@ -0,0 +1,55 @@
+import {
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signOut,
+} from "firebase/auth";
+import { useEffect, useState } from "react";
+import initializeAuthentication from "../firebase/firebase.init";
+
+initializeAuthentication();
+
+const useFirebase = () => {
+  const [user, setUser] = useState({});
+  const [error, setError] = useState("");
+  const auth = getAuth();
+
+  const googleProvider = new GoogleAuthProvider();
+
+  const signInUsingGoogle = () => {
+    signInWithPopup(auth, googleProvider)
+      .then((result) => {
+        setUser(result.user);
+      })
+      .catch((error) => {
+        setError(error.message);
+      });
+  };
+  // observe user state change
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser(user);
+      } else {
+        setUser({});
+      }
+    });
+    return () => unsubscribe;
+  }, []);
+
+  // sign out
+  const logOut = () => {
+    signOut(auth).then(() => {
+      setUser({});
+    });
+  };
+  return {
+    user,
+    error,
+    signInUsingGoogle,
+    logOut,
+  };
+};
+
+export default useFirebase;
