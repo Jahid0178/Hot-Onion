ea7a3bec00e97fa5ae306163ff1bcdf2adcf96fc
Jahid <jahidulhaque767@gmail.com>
2021-10-14 07:49:14 +0600

    add firebase

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDoQy8FC1pFrAGND08Qh1U-S2uLrs2RFh4",
+  authDomain: "red-onion-edbeb.firebaseapp.com",
+  projectId: "red-onion-edbeb",
+  storageBucket: "red-onion-edbeb.appspot.com",
+  messagingSenderId: "112242710708",
+  appId: "1:112242710708:web:b02d6bb82ad9a843bc9fe1",
+};
+
+export default firebaseConfig;
