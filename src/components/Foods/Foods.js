84f1dc309c0d9c70f2da3af938f5b9f4ed58f516
Jahid <jahidulhaque767@gmail.com>
2021-10-15 18:37:51 +0600

    added food category

--- src/components/Foods/Foods.js
+++ src/components/Foods/Foods.js
@@ -1,10 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
+import { Card, Container, Row, Col } from "react-bootstrap";
 import "./Foods.css";
+import Data from "../../data/allData";
 
 const Foods = () => {
+  const [items, setItems] = useState(Data);
+  const filterItem = (categoryItem) => {
+    const updateItem = Data.filter((currentElement) => {
+      return currentElement.category === categoryItem;
+    });
+    setItems(updateItem);
+  };
   return (
     <div className="foods-container">
-      <h2>this is foods</h2>
+      <Container>
+        <div className="d-flex justify-content-evenly mt-5">
+          <button
+            className="btn btn-warning"
+            onClick={() => filterItem("Breakfast")}
+          >
+            Breakfast
+          </button>
+          <button
+            className="btn btn-warning"
+            onClick={() => filterItem("Lunch")}
+          >
+            Lunch
+          </button>
+          <button
+            className="btn btn-warning"
+            onClick={() => filterItem("Dinner")}
+          >
+            Dinner
+          </button>
+        </div>
+        <div className="mt-4">
+          <Row>
+            {items.map((item) => {
+              const { title, price, desc, image, id } = item;
+              return (
+                <Col key={id}>
+                  <Card
+                    style={{ width: "18rem" }}
+                    className="card-container mb-2"
+                  >
+                    <Card.Img variant="top" src={image} alt={title} />
+                    <Card.Body>
+                      <Card.Title>{title}</Card.Title>
+                      <Card.Text>{desc}</Card.Text>
+                      <p>{price}</p>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              );
+            })}
+          </Row>
+        </div>
+      </Container>
     </div>
   );
 };
